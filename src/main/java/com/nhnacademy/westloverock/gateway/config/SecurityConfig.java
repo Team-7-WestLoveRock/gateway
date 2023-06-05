f05f5be175e5b4c6cba0a89fef00bc226cee6e60
koesnam <76582376+10kseok@users.noreply.github.com>
2023-06-06 01:22:03 +0900

    🧠Refactor(SecurityConfig): 설정코드 간소화
    naver OAuth 제거 및 Github OAuth 설정을 application-local.properties로 이전

--- src/main/java/com/nhnacademy/westloverock/gateway/config/SecurityConfig.java
+++ src/main/java/com/nhnacademy/westloverock/gateway/config/SecurityConfig.java
@@ -27,9 +27,8 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(req ->
                         req.anyRequest().authenticated())
-                .oauth2Login(o -> o.clientRegistrationRepository(clientRegistrationRepository())
-                        .authorizedClientService(authorizedClientService()))
                 .formLogin().and()
+                .oauth2Login().and()
                 .logout(l -> l.logoutUrl("/logout")
                         .invalidateHttpSession(true))
                 .csrf().and()
@@ -50,36 +49,5 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        return new InMemoryClientRegistrationRepository(github(), naver());
-    }
-
-    @Bean
-    public OAuth2AuthorizedClientService authorizedClientService() {
-        return new InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository());
-    }
-
-    public ClientRegistration github() {
-        return CommonOAuth2Provider.GITHUB.getBuilder("github")
-                .userNameAttributeName("name")
-                .clientId("c1bdfce87dbf5de8a27b")
-                .clientSecret("6ba9890a56250579be8a32700b3ddf653f8df035")
-                .build();
-    }
-
-    public ClientRegistration naver() {
-        return ClientRegistration.withRegistrationId("naver")
-                .clientId("HDVvfyB9yXd96Hl6B1V6")
-                .clientSecret("ylCfvwj96I")
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .scope("name", "email")
-                .redirectUri("{baseUrl}/{action}/oauth2/code/{registrationId}")
-                .authorizationUri("https://nid.naver.com/oauth2.0/authorize")
-                .tokenUri("https://nid.naver.com/oauth2.0/token")
-                .userInfoUri("https://openapi.naver.com/v1/nid/me")
-                .userNameAttributeName("response")
-                .build();
     }
 }
