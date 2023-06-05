cf501190986c3a391959bf2a486e27f9babefd62
koesnam <76582376+10kseok@users.noreply.github.com>
2023-06-05 10:05:41 +0900

    configuration

---
+++ src/test/java/com/nhnacademy/westloverock/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.westloverock.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("dev")
+@SpringBootTest
+class GatewayApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
