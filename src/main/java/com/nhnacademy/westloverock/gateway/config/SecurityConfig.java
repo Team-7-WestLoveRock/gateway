97e8aa608bfcfea99e63845a74126f8e3655f3d7
koesnam <76582376+10kseok@users.noreply.github.com>
2023-06-06 14:10:07 +0900

    🍎Chore: 패키지 정리
    github oauth 설정으로 생겼던 패키지 정리
    :1234:

--- src/main/java/com/nhnacademy/westloverock/gateway/config/SecurityConfig.java
+++ src/main/java/com/nhnacademy/westloverock/gateway/config/SecurityConfig.java
@@ -5,22 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity(debug = true)
-@EnableMethodSecurity
 @Configuration
 public class SecurityConfig {
     @Bean
